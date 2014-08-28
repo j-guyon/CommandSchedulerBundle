79fd8065bc785ae53976cc471ff54569d8490808
Julien Guyon <julienguyon@hotmail.com>
2014-08-28 17:32:09 +0200

    SensioLabInsight Fix - Unused use and todo

--- Command/ExecuteCommand.php
+++ Command/ExecuteCommand.php
@@ -4,9 +4,7 @@ namespace JMose\CommandSchedulerBundle\Command;
 
 use Cron\CronExpression;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
