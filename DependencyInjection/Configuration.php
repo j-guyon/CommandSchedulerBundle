74286f0088042b15534a28df5ce3550f23323e89
Julien Guyon <julienguyon@hotmail.com>
2014-08-28 17:38:27 +0200

    SensioLabInsight Fix - Unused var

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,11 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('jmose_command_scheduler');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
