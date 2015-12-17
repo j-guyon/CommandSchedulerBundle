bb0420cbb35657605582e8cbe0bb0b0270c43e57
daniel <daniel@skynet>
2015-12-17 21:30:35 +0100

    execution list can be filtered and ordered. found out new version doesn't work with symfony 2.7 - i don't care.

--- Resources/public/js/global.js
+++ Resources/public/js/global.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
         initCronHelper();
     }
 
-    if(document.getElementById('dataTable')){
+    if (document.getElementById('dataTable')) {
         initDataTable();
     }
 });
@@ -206,38 +206,68 @@ function handleVal(value) {
     return result.join(',');
 }
 
+/**
+ * initialize dataTable (list of all executions)
+ */
 function initDataTable() {
     var $table = $('#dataTable'),
         dataTable,
         colIdx = 0;
 
     dataTable = $table.DataTable({
-        ordering: true
+        ordering: true,
+        columnDefs: [
+            {
+                targets: 3,
+                orderable: false
+            }
+        ],
+        order: [
+            [1, "asc"]
+        ]
     });
 
+    buildTableFilter(dataTable, 'commandfilter', 0);
+    buildTableFilter(dataTable, 'datefilter', 1);
+}
+
+/**
+ * build filteroptions for table filter
+ *
+ * @param {object} dataTable datatable object
+ * @param {string} selector id of select field
+ * @param {int} colIdx number of tablecolumn
+ */
+function buildTableFilter(dataTable, selector, colIdx) {
     // Create the select list and search operation
-    var select = $('<select />')
-        .appendTo(
-        dataTable.column(colIdx).footer()
-    )
-        .on( 'change', function () {
-            dataTable
-                .column( colIdx )
-                .search( $(this).val() )
-                .draw();
-        } );
+    var $select = $('#' + selector)
+        .on('change', function () {
+            var col = dataTable.column(colIdx),
+                searchVal = $(this).val(),
+                regexp = false;
+
+            // multiple selections
+            if(typeof searchVal == 'object') {
+                searchVal = searchVal.join('|');
+                regexp = true;
+            }
+
+            // filter column
+            col
+                .search(searchVal, regexp, false) // regexp optional, no smart search
+                .draw(); // refresh table
+        });
 
     // Get the search data for the first column and add to the select list
     dataTable
-        .column( colIdx )
-        .cache( 'search' )
+        .column(colIdx)
+        .cache('search')
         .sort()
         .unique()
-        .each( function ( d ) {
-            select.append( $('<option value="'+d+'">'+d+'</option>') );
-        } );
+        .each(function (d) {
+            $select.append($('<option value="' + d + '">' + d + '</option>'));
+        });
 }
-
 /**
  * initialize runtime and returncode graph
  */
