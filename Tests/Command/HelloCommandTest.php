4defef91dee75f5a5107952367f0d58191a35684
daniel <daniel@skynet>
2015-12-27 12:22:58 +0100

    startet extending tests to get travis running

---
+++ Tests/Command/HelloCommandTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 26.12.15
+ * Time: 16:31
+ */
+
+namespace scheduler\Command;
+
+
+class HelloCommandTest extends \PHPUnit_Framework_TestCase {
+
+}
---
+++ Tests/Command/MonitorCommandTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 26.12.15
+ * Time: 16:32
+ */
+
+namespace scheduler\Command;
+
+
+class MonitorCommandTest extends \PHPUnit_Framework_TestCase {
+
+}
