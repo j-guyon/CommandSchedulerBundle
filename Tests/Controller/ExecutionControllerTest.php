0b32b930dc265f35fb49c75974324c68a6224812
daniel <daniel@skynet>
2016-02-07 12:25:04 +0100

    fixed touchpad typo - arrgl

--- Tests/Controller/ExecutionControllerTest.php
+++ Tests/Controller/ExecutionControllerTest.php
@@ -12,47 +12,7 @@ class ExecutionControllerTest extends CommandSchedulerBaseTest
 {
     protected $rowSelector = 'tr.execution';
     protected $graphSelector = 'div.graphs';
-
-    /**
-     * test list of executions without any data
-     */
-    public function testCommandExecutionsEmpty()
-    {
-        $crawler = $this->loadPage(5);
-
-        $result = $crawler->filter($this->rowSelector)->count();
-        $this->assertEquals(0, $result);
-    }
-
-    /**
-     * test list of executions of a single command without executions
-     */
-    public function testCommandExecutionsWithoutExecutions()
-    {
-        $this->loadDataFixtures();
-
-        $crawler = $this->loadPage(1);
-
-        $result = $crawler->filter($this->rowSelector)->count();
-        $this->assertEquals(0, $result);
-    }
-
-    /**
-     * test list of executions of a single command with executions
-     */
-    public function testCommandExecutions()
-    {
-        $this->loadDataFixtures();
-
-        $crawler = $this->loadPage(5);
-
-        $executions = $crawler->filter($this->rowSelector)->count();
-        $graphs = $crawler->filter($this->graphSelector)->count();
-
-        $this->assertEquals(NUMBER_EXECUTIONS, $executions);
-        $this->assertEquals(2, $graphs);
-    }
-
+    
     /**
      * Test ajax call to show the output for a given execution id
      */
