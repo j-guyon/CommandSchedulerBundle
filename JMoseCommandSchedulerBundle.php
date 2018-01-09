58cde19a52c0de44080544443416a288ebc64860
den <denis.vinokurov@gmail.com>
2018-01-09 18:24:45 +0300

    Fix code style

--- JMoseCommandSchedulerBundle.php
+++ JMoseCommandSchedulerBundle.php
@@ -11,7 +11,8 @@ class JMoseCommandSchedulerBundle extends Bundle
      * {@inheritdoc}
      * @return JMoseCommandSchedulerExtension
      */
-    public function getContainerExtension() {
+    public function getContainerExtension()
+    {
         $class = $this->getContainerExtensionClass();
         return new $class;
     }
@@ -21,6 +22,6 @@ class JMoseCommandSchedulerBundle extends Bundle
      */
     protected function getContainerExtensionClass()
     {
-        return 'JMose\CommandSchedulerBundle\DependencyInjection\JMoseCommandSchedulerExtension';
+        return JMoseCommandSchedulerExtension::class;
     }
 }
