95f6809f1401505e083d0047c0ab9a45d8d3cd35
Christoph Singer <singer@webagentur72.de>
2019-07-19 12:04:26 +0200

    Added scheduler daemon

---
+++ Tests/Command/StartStopSchedulerCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JMose\CommandSchedulerBundle\Tests\Command;
+
+use JMose\CommandSchedulerBundle\Command\StartSchedulerCommand;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+
+class StartStopSchedulerCommandTest extends WebTestCase
+{
+
+    /**
+     * Test scheduler:start and scheduler:stop
+     */
+    public function testStartAndStopScheduler()
+    {
+        //DataFixtures create 4 records
+        $this->loadFixtures(
+            array(
+                'JMose\CommandSchedulerBundle\Fixtures\ORM\LoadScheduledCommandData'
+            )
+        );
+
+        $pidFile = sys_get_temp_dir().DIRECTORY_SEPARATOR.StartSchedulerCommand::PID_FILE;
+
+
+        $output = $this->runCommand('scheduler:start');
+        $this->assertStringStartsWith('Command scheduler started in non-blocking mode...', $output);
+        $this->assertFileExists($pidFile);
+
+        $output = $this->runCommand('scheduler:stop');
+        $this->assertStringStartsWith('Command scheduler is stopped.', $output);
+        $this->assertFileNotExists($pidFile);
+    }
+
+}
