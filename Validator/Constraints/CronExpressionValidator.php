17126932466d26dd6b189bfb965bd1e48c72249a
Javier Spagnoletti <phansys@gmail.com>
2020-01-28 13:41:08 -0300

    "mtdowling/cron-expression" is abandoned

--- Validator/Constraints/CronExpressionValidator.php
+++ Validator/Constraints/CronExpressionValidator.php
@@ -22,18 +22,25 @@ class CronExpressionValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        $value = (string)$value;
+        $value = (string) $value;
 
-        if (null === $value || '' === $value) {
+        if ('' === $value) {
             return;
         }
 
         try {
             CronExpressionLib::factory($value);
         } catch (\InvalidArgumentException $e) {
+            // This condition is required in order to respect BC with "mtdowling/cron-expression".
+            // It must be removed at 3.0 release.
+            // @see https://github.com/mtdowling/cron-expression/commit/56e89730e60a0e945bf4ea10c48b80a406c7e7a0.
+            if ('6 is not a valid position' === $e->getMessage()) {
+                @trigger_error($e->getMessage().' and its support is deprecated since jmose/command-scheduler-bundle 2.x.', E_USER_DEPRECATED);
+
+                return;
+            }
+
             $this->context->addViolation($constraint->message, array(), $value);
         }
-
-        return;
     }
 }
